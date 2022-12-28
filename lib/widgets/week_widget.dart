65f365b92aaec35e660c1f2bd982c70762c3b1f6
Dalal Alsabah <dasabah@gmail.com>
2022-12-28 12:21:44 +0300

    listed tickets working

---
+++ lib/widgets/week_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ticket_app/models/event.dart';
+
+class WeekEvent1 extends StatelessWidget {
+  WeekEvent1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 200,
+      child: Container(
+        height: 10,
+        child: Card(
+          child: InkWell(
+            onTap: () {
+              context.push('');
+              print('tapped events');
+            },
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 99,
+                  width: 300,
+                  child: Image(
+                    fit: BoxFit.contain,
+                    image: AssetImage('lib/assets/newyearwalk.jpeg'),
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    "Jan 1st Marathon",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+                Text('Souq Sharq'),
+                Text('Kuwait'),
+                Text('2023-01-01')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
